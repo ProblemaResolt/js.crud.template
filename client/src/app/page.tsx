4317969211d2adc9af09b365a4a93aeb8f92ba4d
boner.engineer <boner.engineer@outlook.jp>
2023-10-04 15:53:55 +0900

    Storybook move

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,23 @@
 "use client"
-import TodoList from "./TodoList";
+import React from 'react';
+import { TodoList } from "../stories/Molecules/TodoList";
+import { Header } from "../stories/Organisms/Header";
+
+type User = {
+  name: string;
+};
 
 export default function Home() {
+  const [user, setUser] = React.useState<User>();
 
   return (
     <main>
+      <Header
+        user={user}
+        onLogin={() => setUser({ name: 'Jane Doe' })}
+        onLogout={() => setUser(undefined)}
+        onCreateAccount={() => setUser({ name: 'Jane Doe' })}
+  />
       <TodoList />
     </main>
   )

db1be72900f2fbde795e15bdcfbe6d7b17072e8c
boner.engineer <boner.engineer@outlook.jp>
2023-10-12 01:18:01 +0900

    user data seed

---
+++ api/prisma/migrations/20231010055836_init/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE `Todo` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `completed` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NULL,
+    `published` BOOLEAN NOT NULL DEFAULT false,
+    `authorId` INTEGER NULL,
+
+    INDEX `main_index`(`title`, `content`),
+    UNIQUE INDEX `Post_authorId_title_key`(`authorId`, `title`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER',
+
+    UNIQUE INDEX `Users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `Users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
--- api/prisma/seeds.ts
+++ api/prisma/seeds.ts
@@ -1,34 +1,57 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const task1 = await prisma.todo.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
-      id: 1,
-      title: "Task 1",
-    },
-  });
-  const task2 = await prisma.todo.upsert({
-    where: { id: 2 },
-    update: {},
-    create: {
-      id: 2,
-      title: "Task 2",
-    },
-  });
+async function seedDatabase() {
+  try {
+    // パスワードのハッシュ化
+    const hashedPassword = await bcrypt.hash('your_password_here', 10); // パスワードをハッシュ化
 
-  console.log({ task1, task2 });
+    const user1 = await prisma.users.create({
+      data: {
+        email: 'user1@example.com',
+        name: 'User 1',
+        password: hashedPassword, // ハッシュ化したパスワードを保存
+        role: 'USER',
+      },
+    });
+    
+    const user2 = await prisma.users.create({
+      data: {
+        email: 'user2@example.com', // 異なるメールアドレスを使用
+        name: 'User 2',
+        password: hashedPassword, // ハッシュ化したパスワードを保存
+        role: 'ADMIN',
+      },
+    });
+
+    // Post モデルにデータを作成
+    const post1 = await prisma.post.create({
+      data: {
+        title: 'First Post',
+        content: 'This is the content of the first post.',
+        published: true,
+        authorId: user1.id, // user1 の ID を指定
+      },
+    });
+
+    // Post モデルにデータを作成
+    const post2 = await prisma.post.create({
+      data: {
+        title: 'second Post',
+        content: 'This is the content of the first post.',
+        published: true,
+        authorId: user2.id, // user2 の ID を指定
+      },
+    });
+
+    console.log('シードデータの作成が完了しました。');
+  } catch (error) {
+    console.error('シードデータの作成中にエラーが発生しました:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+seedDatabase();

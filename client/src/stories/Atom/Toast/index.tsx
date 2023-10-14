066aee1ca8fb1e437887db090419deab743b366d
boner.engineer <boner.engineer@outlook.jp>
2023-10-15 08:52:22 +0900

    Toast Add

---
+++ client/src/stories/Atom/Toast/index.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export const Toast = ({ message, type }:any) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClose = () => {
+    setIsVisible(false);
+  };
+
+  return (
+    <div
+      className={`toast ${type}`}
+      style={{ display: isVisible ? "block" : "none" }}
+    >
+      <div>
+        {message}
+      </div>
+      <button onClick={handleClose}>閉じる</button>
+    </div>
+  );
+};
--- client/src/stories/Molecules/Auth/index.tsx
+++ client/src/stories/Molecules/Auth/index.tsx
@@ -3,20 +3,23 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "../../Atom/Button";
 import { Modal } from "../../Atom/Modal";
+import { Toast } from "../../Atom/Toast";
+import axios from "axios";
 
 type User = {
-    name: string;
-  };
-  
+  name: string;
+};
+
 interface AuthProps {
-    user?: User;
-    onLogin: () => void;
-    onLogout: () => void;
-    onCreateAccount: () => void;
-  }
+  user?: User;
+  onLogin: () => void;
+  onLogout: () => void;
+  onCreateAccount: () => void;
+}
 
 export const Auth = ({ user, onLogin, onLogout, onCreateAccount }: AuthProps) => {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [showToast, setShowToast] = useState(false);
 
   const handleLogin = () => {
     setIsLoginModalOpen(true);
@@ -24,6 +27,27 @@ export const Auth = ({ user, onLogin, onLogout, onCreateAccount }: AuthProps) =>
 
   const handleLoginModalClose = () => {
     setIsLoginModalOpen(false);
+  };
+
+  // `form`タグの`onSubmit`属性に、モーダルを閉じないようにするコードを追加します。
+  const handleResetPassword = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // APIを呼び出します。
+    const url = `/api/v1/users/password/reset`;
+    const data = { email: event.currentTarget.elements[0].value };
+    axios.post(url, data).then((response:any) => {
+      // APIから応答が返ってきたら、応答を検証します。
+      const result = response.data;
+      if (result.success) {
+        // 応答が成功の場合、モーダルを閉じて、トーストを表示します。
+        setIsLoginModalOpen(false);
+        Toast.show(result.message, "success");
+      } else {
+        // 応答が失敗の場合、エラーメッセージを表示します。
+        Toast.show(result.message, "error");
+      }
+    });
   };
 
   return (
@@ -35,10 +59,15 @@ export const Auth = ({ user, onLogin, onLogout, onCreateAccount }: AuthProps) =>
           isOpen={isLoginModalOpen}
           onClose={handleLoginModalClose}
         >
-          <form onSubmit={handleLogin}>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button type="submit">Login</button>
+          <form onSubmit={handleResetPassword}>
+            <input
+              type="email"
+              placeholder="Email"
+              style={{ margin: 0 }}
+              onClick={(event) => event.stopPropagation()}
+            />
+            <input type="password" placeholder="Password" onClick={(event) => event.stopPropagation()}/>
+            <button type="submit" onClick={(event) => event.stopPropagation()}>Reset Password</button>
           </form>
         </Modal>
       )}

dc08902fd5e052c18cd3faae6f3f5189df22f58e
boner.engineer <boner.engineer@outlook.jp>
2023-10-14 09:19:27 +0900

    Backend Auth add

--- api/src/routes/auth.route.ts
+++ api/src/routes/auth.route.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 import passport from "passport";
 import type { User } from ".prisma/client";
 import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
+import crypto from "crypto";
+
 import prisma from "../config/prisma";
 import { appConfig } from "../config/app";
 import { encryptPassword } from "../utils/password";
@@ -78,4 +81,131 @@ router.post("/login", async (req, res, next) => {
     });
 });
 
+router.post("/forgot-password", async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).json({
+      status: false,
+      message: "Email is required!",
+    });
+  }
+
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    return res.status(404).json({
+      status: false,
+      message: "User not found!",
+    });
+  }
+
+  // 認証コードを生成
+  const authCode = generateAuthCode();
+
+  const defaults = {
+    service: "SMTP",
+    host: "smtp.example.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "username",
+      password: "password",
+    },
+  };
+  
+  const transporter = nodemailer.createTransport({
+  ...defaults,
+});
+  
+
+  const mailOptions = {
+    from: "from@example.com",
+    to: email,
+    subject: "パスワード再設定",
+    text: `
+      パスワードを再設定するリンクを送信しました。
+
+      <a href="https://example.com/reset-password?authCode=${authCode}">パスワードを再設定する</a>
+    `,
+  };
+
+  await transporter.sendMail(mailOptions);
+
+  return res.status(200).json({
+    status: true,
+    message: "認証コードを送信しました。",
+  });
+});
+
+router.post("/reset-password", async (req, res) => {
+  const { email, authCode, password } = req.body;
+
+  if (!email || !authCode || !password) {
+    return res.status(400).json({
+      status: false,
+      message: "Email, authCode, password are required!",
+    });
+  }
+
+  if (!await verifyAuthCode(email, authCode)) {
+    return res.status(401).json({
+      status: false,
+      message: "Invalid authCode!",
+    });
+  }
+
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    return res.status(404).json({
+      status: false,
+      message: "User not found!",
+    });
+  }
+
+  await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      password: await encryptPassword(password),
+    },
+  });
+
+  return res.status(200).json({
+    status: true,
+    message: "パスワードを変更しました。",
+  });
+});
+
 export default router;
+
+
+function generateAuthCode() {
+  const randomBytes = crypto.randomBytes(16);
+  const authCode = randomBytes.toString("hex");
+
+  // 認証コードの有効期限を設定
+  const now = new Date();
+  const expiredAt = new Date(now.getTime() + 60 * 60 * 1000); // 1時間後に有効期限切れ
+
+  return { authCode, expiredAt };
+}
+
+async function verifyAuthCode(email: string, authCode: string) {
+  // データベースから認証コードを取得する
+  const user = await prisma.user.findUnique({ where: { email } });
+  const authCodeFromDB = user.authCode;
+
+  // 認証コードの有効期限を確認する
+  const now = new Date();
+  const expiredAt = new Date(user.authCodeExpiredAt);
+
+  if (now > expiredAt) {
+    return false;
+  }
+
+  // 取得した認証コードと引数のauthCodeが一致すれば、認証コードが有効であると見なす
+  if (authCodeFromDB === authCode) {
+    return true;
+  } else {
+    return false;
+  }
+}
